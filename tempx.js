b0b34cfca8b2aa813fb4eec94a96021f3dc3de5d
mserag1337 <mohamed_serageldin@cysiv.com>
2021-12-22 20:59:56 +0200

    asd

--- tempx.js
+++ tempx.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const core = require('@actions/core');
 
 let originalCIMRawdata = fs.readFileSync('file_1.json');
 let originalCIM = JSON.parse(originalCIMRawdata);
@@ -10,14 +11,23 @@ let updatedCIM = JSON.parse(updatedCIMRawdata);
 
 var originalKeys = Object.keys(originalCIM);
 
+let hasErrors = false;
+let fieldNameErrors = []
+let supportArrayErrors = []
 for (let i = 0; i < originalKeys.length; i++) {
     const key = originalKeys[i];
     const originalCIMObject = originalCIM[key];
     const updatedCIMObject = updatedCIM[key];
     if (originalCIMObject['data_type'] != updatedCIMObject['data_type']) {
-        console.log(`field \'${key}\' changed from \'${originalCIMObject['data_type']}\' to \'${updatedCIMObject['data_type']}\'`)
+        hasErrors = true;
+        fieldNameErrors.push(`field \'${key}\' changed from \'${originalCIMObject['data_type']}\' to \'${updatedCIMObject['data_type']}\'`);
     }
     if (originalCIMObject['supports_array'] != updatedCIMObject['supports_array']) {
-        console.log(`field \'${key}\' changed \'supports_array\' from \'${originalCIMObject['supports_array']}\' to \'${updatedCIMObject['supports_array']}\'`)
+        hasErrors = true;
+        supportArrayErrors.push(`field \'${key}\' changed \'supports_array\' from \'${originalCIMObject['supports_array']}\' to \'${updatedCIMObject['supports_array']}\'`)
     }
+}
+
+if (hasErrors) {
+    core.setFailed(`Action failed with these errors ${fieldNameErrors} / ${supportArrayErrors}`);
 }
