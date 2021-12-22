e824da5d43eb3f260363c3c5cafc340c61503ed2
mserag1337 <mohamed_serageldin@cysiv.com>
2021-12-22 18:48:23 +0200

    asd

--- tempx.js
+++ tempx.js
@@ -1,1 +1,22 @@
-console.log('This example is different!');
+'use strict';
+
+const fs = require('fs');
+
+let originalCIMRawdata = fs.readFileSync('file_1.json');
+let originalCIM = JSON.parse(originalCIMRawdata);
+
+let updatedCIMRawdata = fs.readFileSync('file_1.latest.json');
+let updatedCIM = JSON.parse(updatedCIMRawdata);
+
+var originalKeys = Object.keys(originalCIM);
+
+for (let i = 0; i < originalKeys.length; i++) {
+    const key = originalKeys[i];
+    const originalCIMObject = originalCIM[key];
+    const updatedCIMObject = updatedCIM[key];
+    if (originalCIMObject['data_type'] != updatedCIMObject['data_type']) {
+        console.log(`field \'${key}\' changed from \'${originalCIMObject['data_type']}\' to \'${updatedCIMObject['data_type']}\'`)
+    } else if (originalCIMObject['supports_array'] != updatedCIMObject['supports_array']) {
+        console.log(`field \'${key}\' changed \'supports_array\' from \'${originalCIMObject['supports_array']}\' to \'${updatedCIMObject['supports_array']}\'`)
+    }
+}
