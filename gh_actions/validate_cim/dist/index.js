177250de3cea37b0287c84f2e06f0c122632baca
mserag1337 <mohamed_serageldin@cysiv.com>
2021-12-23 19:50:34 +0200

    as

--- gh_actions/validate_cim/dist/index.js
+++ gh_actions/validate_cim/dist/index.js
@@ -415,30 +415,16 @@ async function run() {
     let updatedCIM = JSON.parse(updatedCIMRawdata);
 
     var originalKeys = Object.keys(originalCIM);
-
-    let hasErrors = false;
-    let fieldNameErrors = []
-    let supportArrayErrors = []
     for (let i = 0; i < originalKeys.length; i++) {
         const key = originalKeys[i];
         const originalCIMObject = originalCIM[key];
         const updatedCIMObject = updatedCIM[key];
         if (originalCIMObject['data_type'] != updatedCIMObject['data_type']) {
-            hasErrors = true;
-            fieldNameErrors.push(`field \'${key}\' changed from \'${originalCIMObject['data_type']}\' to \'${updatedCIMObject['data_type']}\'`);
+            core.setFailed(`field \'${key}\' changed from \'${originalCIMObject['data_type']}\' to \'${updatedCIMObject['data_type']}\'`);
         }
         if (originalCIMObject['supports_array'] != updatedCIMObject['supports_array']) {
-            hasErrors = true;
-            supportArrayErrors.push(`field \'${key}\' changed \'supports_array\' from \'${originalCIMObject['supports_array']}\' to \'${updatedCIMObject['supports_array']}\'`)
+            core.setFailed(`field \'${key}\' changed \'supports_array\' from \'${originalCIMObject['supports_array']}\' to \'${updatedCIMObject['supports_array']}\'`)
         }
-    }
-
-    if (hasErrors) {
-        for (let index = 0; index < fieldNameErrors.length; index++) {
-            const element = fieldNameErrors[index];
-            core.setFailed(element);
-        }
-        // core.setFailed(`Action failed with these errors ${fieldNameErrors} / ${supportArrayErrors}`);
     }
 }
 
